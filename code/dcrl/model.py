3505c628bbc953c5878a1cb4fbe877874ff835c4
Minh Hieu Nong <mnong@virtuosgames.com>
2024-08-08 17:39:01 +0800

    feat: Checkpoint

--- code/dcrl/model.py
+++ code/dcrl/model.py
@@ -1,7 +1,10 @@
-import tqdm
 import torch
-import numpy as np
 import torch.nn as nn
+import torch.nn.functional as F
+
+import tqdm
+import numpy as np
+
 from norms import frobenius_norm, l21_norm, spectral_norm 
 from common import get_default_device, apply_model_to_batch
 
@@ -105,32 +108,214 @@ def logistic_loss(y, y_positive, y_negatives):
     return loss
 
 # Compute Yunwen's complexity measure
-def compute_complexity_FRO(dataloader, network: Net):
+def compute_complexity_YW(dataloader, network: Net, device=None):
     # Report
     print('[INFO] Computing Yunwen et. al. complexity measure...')
+
+    # Get device
+    if device is None:
+        device = get_default_device()
+        network = network.to(device)
+        network.device = device
 
     # Get necessary constants
     L = network.L 
     n = len(dataloader)
     d = network.out_dim
+
+    # Initialization
+    B_x = 0.0
+    complexity = np.sqrt(L * d)
     
     # Compute complexity
-    complexity = np.sqrt(L * d)
     for l in range(1, L+1):
         A_l = network._get_v_layer_weights(layer=l)
         complexity *= frobenius_norm(A_l)
         complexity *= spectral_norm(A_l)
-    complexity = complexity / np.sqrt(n)
 
     # Find B_x
-    B_x = 0.0
-    with tqdm.tqdm(total=N) as pbar:
+    print('[INFO] Computing B_x...')
+    network.eval()
+    with tqdm.tqdm(total=n) as pbar:
         for i, batch in enumerate(dataloader):
-            # Calculate loss
-            y1, y2, y3 = apply_model_to_batch(network, batch, device=network.device)
-            loss = logistic_loss(y1, y2, y3)
+            # Calculate inputs l2 norms
+            X = torch.cat([batch[0], batch[1], *batch[2]], dim=0)
+            X_l2 = torch.linalg.norm(X, dim=1, ord=2).squeeze()
+            X_l2_max = torch.max(X_l2)
+
+            # Check
+            if B_x < X_l2_max.item():
+                B_x = X_l2_max.item()
 
             # Update progress
+            pbar.set_postfix({
+                'batch' : f'#[{i+1}/{n}]',
+                'current Bx' : f'{B_x:.4f}'
+            })
+            pbar.update(1)
+    complexity = complexity * (B_x ** 2)
+    complexity = complexity / np.sqrt(n)
+    return complexity
+
+# Compute our complexity measure (Thm. 1)
+def compute_complexity_THM1(dataloader, network: Net, device=None):
+    # Report
+    print('[INFO] Computing our (THM. 1) complexity measure...')
+
+    # Get device
+    if device is None:
+        device = get_default_device()
+        network = network.to(device)
+        network.device = device
+
+    # Get necessary data
+    L = network.L
+    n = len(dataloader) 
+
+    # Initialization
+    complexity = 1.0 
+    B_x = 0.0
+
+    # Compute product of spectral norms
+    sum_alsl = 0.0
+    for l in range(1, L+1):
+        A_l = network._get_v_layer_weights(layer=l)
+        s_l = spectral_norm(A_l)
+        a_l = l21_norm(A_l)
+        complexity *= (s_l ** 2) 
+        sum_alsl += ((a_l / s_l) ** (2/3))
+    complexity *= (sum_alsl ** (3/2))
+
+    # Compute R and B_x
+    print('[INFO] Computing B_x...')
+    network.eval()
+    with tqdm.tqdm(total=n) as pbar:
+        for i, batch in enumerate(dataloader):
+            # Calculate inputs l2 norms
+            X = torch.cat([batch[0], batch[1], *batch[2]], dim=0)
+            X_l2 = torch.linalg.norm(X, dim=1, ord=2).squeeze()
+            X_l2_max = torch.max(X_l2)
+
+            # Check
+            if B_x < X_l2_max.item():
+                B_x = X_l2_max.item()
+
+            # Update progress
+            pbar.set_postfix({
+                'batch' : f'#[{i+1}/{n}]',
+                'current Bx' : f'{B_x:.4f}'
+            })
             pbar.update(1)
 
+    complexity = complexity * (B_x ** 2)
+    complexity = complexity / np.sqrt(n)
+    return complexity
+
+# Compute our complexity measure (Thm. 2)
+def compute_complexity_THM2(dataloader, network: Net, device=None):
+    # Report
+    print('[INFO] Computing our (THM. 2) complexity measure...')
+
+    # Get device
+    if device is None:
+        device = get_default_device()
+        network = network.to(device)
+        network.device = device
+
+    # Get necessary data
+    L = network.L
+    n = len(dataloader) 
+
+    # Initialization
+    complexity = 1.0 
+    B_x = 0.0
+    R   = 0.0
+
+    # Compute product of spectral norms
+    sum_alsl = 0.0
+    for l in range(1, L+1):
+        A_l = network._get_v_layer_weights(layer=l)
+        s_l = spectral_norm(A_l)
+        a_l = l21_norm(A_l)
+        complexity *= s_l 
+        sum_alsl += ((a_l / s_l) ** (2/3))
+    complexity *= (sum_alsl ** (3/2))
+
+    # Compute R and B_x
+    print('[INFO] Computing B_x and R...')
+    network.eval()
+    with tqdm.tqdm(total=n) as pbar:
+        for i, batch in enumerate(dataloader):
+            # Calculate inputs l2 norms
+            X = torch.cat([batch[0], batch[1], *batch[2]], dim=0)
+            X_l2 = torch.linalg.norm(X, dim=1, ord=2).squeeze()
+            X_l2_max = torch.max(X_l2)
+
+            # Calculate network output
+            y1, y2, y3 = apply_model_to_batch(network, batch, device=network.device)
+            Y = torch.cat([y1, y2, *y3], dim=0)
+            Y_l2 = torch.linalg.norm(Y, dim=1, ord=2).squeeze()
+            Y_l2_max = torch.max(Y_l2)
+
+            # Check
+            if B_x < X_l2_max.item():
+                B_x = X_l2_max.item()
+
+            if R < Y_l2_max.item():
+                R = Y_l2_max.item()
+
+            # Update progress
+            pbar.set_postfix({
+                'batch' : f'#[{i+1}/{n}]',
+                'current Bx' : f'{B_x:.4f}',
+                'current R' : f'{R:.4f}'
+            })
+            pbar.update(1)
+
+    complexity = complexity * B_x * R
+    complexity = complexity / np.sqrt(n)
+    return complexity
+
+# Compute our complexity measure (Thm. 3)
+def compute_complexity_THM2(dataloader, network: Net, device=None):
+    # Report
+    print('[INFO] Computing our (THM. 3) complexity measure...')
+
+    # Get device
+    if device is None:
+        device = get_default_device()
+        network = network.to(device)
+        network.device = device
+
+    # Get necessary data
+    L = network.L
+    n = len(dataloader) 
+
+    # Initialization
+    complexity = 1.0 
+    b_l = [0.0 for l in range(L)]
+
+    # Compute all a_l
+
+    # Compute R and B_x
+    print('[INFO] Computing b_L and R...')
+    network.eval()
+    with tqdm.tqdm(total=n) as pbar:
+        for i, batch in enumerate(dataloader):
+            # Calculate inputs l2 norms
+            X = torch.cat([batch[0], batch[1], *batch[2]], dim=0)
+            X_l2 = torch.linalg.norm(X, dim=1, ord=2).squeeze()
+            X_l2_max = torch.max(X_l2)
+
+            # Calculate network output
+            y1, y2, y3 = apply_model_to_batch(network, batch, device=network.device)
+            Y = torch.cat([y1, y2, *y3], dim=0)
+            Y_l2 = torch.linalg.norm(Y, dim=1, ord=2).squeeze()
+            Y_l2_max = torch.max(Y_l2)
+
+            # Check
+            if b_l[0] < X_l2_max.item():
+                b_l[0] = X_l2_max.item()
+
+    complexity = complexity / np.sqrt(n)
     return complexity
